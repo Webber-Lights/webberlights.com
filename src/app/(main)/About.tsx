c5286ce4607ff0a7e45c695f34035d5300c7ffa8
Lucas Webber <lucas@lucaswebber.dev>
2025-11-19 14:20:38 -0500

    fix(about): update FM frequency from 97.1 to 89.5

--- src/app/(main)/About.tsx
+++ src/app/(main)/About.tsx
@@ -10,7 +10,7 @@ function About() {
           <h2 className='text-4xl font-bold text-red-600 '>Visit us Nightly<br />Nov 29 - Jan 6</h2>
           <h2 className='text-2xl mt-6'>Monday - Thursday: 5 - 9:30PM</h2>
           <h2 className='text-2xl'>Friday - Sunday: 5 - 10PM</h2>
-          <div className='mt-6 uppercase'>TUNE TO FM 97.1 TO LISTEN</div>
+          <div className='mt-6 uppercase'>TUNE TO FM 89.5 TO LISTEN</div>
       
         </div>
         <div className='md:w-1/2'>
